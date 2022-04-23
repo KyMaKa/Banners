c190b866ed7cd677b09b346d5e166a97d6dba3c8
kymaka <seafth345@gmail.com>
2022-04-23 15:25:32 +0700

    For testing purposes only

--- AdBanners/src/main/java/testtask/banners/data/repository/CategoryRepository.java
+++ AdBanners/src/main/java/testtask/banners/data/repository/CategoryRepository.java
@@ -1,13 +1,12 @@
 package testtask.banners.data.repository;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import testtask.banners.data.models.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
   List<Category> findCategoriesByDeletedFalse();
-  Optional<Category> findCategoryByIdAndDeletedFalse(Long id);
+  Category findCategoryByIdAndDeletedFalse(Long id);
   Category getCategoryByNameAndDeletedFalse(String name);
 
 }
